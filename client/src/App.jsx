cc5dda66987d403b8e729b77238011186a0d248d
Abhishek kumar <75746890+Abhisheek88@users.noreply.github.com>
2024-12-15 12:53:44 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-    <div className="my-20 mx-auto w-[720px]  text-center "> <h1 className='text-yellow-300 font-bold text-3x'>Hi</h1> </div>
+    <div className="my-20 mx-auto w-[720px]  text-center "> <h1 className='text-yellow-100 font-bold text-3x'>Hi</h1> </div>
     </>
   )
 }
