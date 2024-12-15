8c6063658908fefdc8426731dc68a539f4137b32
Abhishek Kumar <ak7739511@gmail.com>
2024-12-15 12:34:20 +0530

    New_project

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+ 
+
+  return (
+    <>
+    <div className="my-20 mx-auto w-[720px]  text-center "> <h1 className='text-yellow-300 font-bold text-3x'>Hi</h1> </div>
+    </>
+  )
+}
+
+export default App
---
+++ server/server.js
@@ -0,0 +1,43 @@
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+
+//create a database connection -> u can also
+//create a separate file for this and then import/use that file here
+
+
+mongoose
+  .connect('mongodb+srv://Abhisheek88:Dell123@abhisheek88.7hlfr.mongodb.net/')
+  .then(() => console.log("MongoDB connected"))
+  .catch((error) => console.log("MongoDB is Not connected"));
+ 
+
+  const app =express();
+  const PORT = process.env.PORT || 5000;
+
+  app.use(
+    cors({
+        origin: 'http://localhost:5173/',
+        methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
+    })
+  );
+
+   
+  app.use(cookieParser)
+  app.use(express.json)
+
+
+  app.listen(PORT, ()=> console.log(`Server is now running on port ${PORT}`) );
+
+ 
+
