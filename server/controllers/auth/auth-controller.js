f95230f338bb5d51dcdb2da1d3b3f3ce4bcaff16
Abhishek Kumar <ak7739511@gmail.com>
2024-12-17 23:15:08 +0530

    login

---
+++ server/controllers/auth/auth-controller.js
@@ -0,0 +1,64 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../../models/User");
+
+//register
+const registerUser =async (req, res)=>{
+
+    const {userName, email,password} =req.body;
+
+
+    try{
+        const hashPassword = await bcrypt.hash(password,12);
+        const newUser =new User({
+            userName,
+            email,
+            password:hashPassword,
+        })
+
+        await newUser.save();
+        res.status(200).json({
+            success: true,
+            message:"Registration successful",
+        })
+    
+    }catch(e){
+        Cconsole.log(e);
+        res.status(500).json({
+            success:false,
+            message:"Some error Occured",
+        })
+    }
+};
+
+
+
+
+//login
+
+const loginUser =async (req, res)=>{
+
+    const {userName, email,password} =req.body;
+
+
+    try{
+    
+    }catch(e){
+        Cconsole.log(e);
+        res.status(500).json({
+            success:false,
+            message:"Some error Occured",
+        })
+    }
+};
+
+
+
+//logout
+
+
+//authMiddleware
+
+
+
+module.exports ={registerUser};
